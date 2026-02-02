e00be50dd4b1bcc1dc393f2fe28cd18038676ea7
sumzbrod <sumzbrod@gmail.com>
2026-02-02 10:09:27 +0300

    klastering

--- tg_screenshot.py
+++ tg_screenshot.py
@@ -3,6 +3,55 @@ import asyncio
 import random
 import subprocess
 from telethon import TelegramClient
+from PIL import Image
+import numpy as np
+import timm
+from torchvision import transforms
+import torch
+
+claster_cats=[
+    0,#0
+    0,#1
+    0,#2
+    1,#3
+    1,#4
+    1,#5
+    1,#6
+    0,#7
+    1,#8
+    1,#9
+]
+with open("K_centrs.npy", 'rb') as f:
+    K_centrs = np.load(f)
+device = "cuda"
+
+model = timm.create_model(
+    "vit_base_patch14_dinov2",
+    pretrained=True,
+    num_classes=0
+).to(device).eval()
+
+transform = transforms.Compose([
+    transforms.Resize(518),
+    transforms.CenterCrop(518),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=(0.485, 0.456, 0.406),
+        std=(0.229, 0.224, 0.225),
+    ),
+])
+
+def embed_image(path):
+    img = Image.open(path).convert("RGB")
+    x = transform(img).unsqueeze(0).to(device)
+    with torch.no_grad():
+        return model(x).cpu().numpy()[0]
+
+def get_claster_cat(path):
+    p = embed_image(path)
+    argmin = int(np.argmin([np.linalg.norm(k - p) for k in K_centrs]))
+    return claster_cats[argmin]
+
 
 # ===== ENV =====
 api_id = int(os.getenv("TG_API_ID"))
@@ -10,9 +59,11 @@ api_hash = os.getenv("TG_API_HASH")
 channel_id = int(os.getenv("TG_CHANNEL_ID"))
 
 BASE_INTERVAL = 15 * 60
-RANDOM_SHIFT = 5 * 60
-SCREEN_PATH = "/tmp/tg_screen.png"
+RANDOM_SHIFT = 13 * 60
+SCREEN_PATH = "/tmp/tg_screen2.png"
 SESSION = os.environ.get("XDG_SESSION_TYPE", "x11")
+MIN_SCREENSHOT_SIZE = 100 * 1024   # 100 KB
+RETRY_DELAY = 5 * 60               # 5 минут
 
 client = TelegramClient(
     "/home/user/Files/utils/session",
@@ -58,15 +109,47 @@ def get_open_windows():
         else get_windows_x11()
     )
 
+
+def is_screenshot_valid(path):
+    # файл существует
+    if not os.path.exists(path):
+        return False
+
+    # проверка размера
+    if os.path.getsize(path) < MIN_SCREENSHOT_SIZE:
+        return False
+
+    # проверка разрешения (на всякий случай)
+    try:
+        with Image.open(path) as img:
+            w, h = img.size
+            if w < 100 or h < 100:
+                return False
+    except Exception:
+        return False
+
+    return True
+
 # ---------- MAIN ----------
 async def main():
     await client.start()
     INCRIMINANT = 0
-
+    BREAK_MESSEAGE_ALREADY_SENTED = False
     while True:
         screenshot = make_screenshot()
-
-        caption = f"#{INCRIMINANT}"
+        if not is_screenshot_valid(screenshot):
+            if os.path.exists(screenshot):
+                os.remove(screenshot)
+            if not BREAK_MESSEAGE_ALREADY_SENTED:
+                await client.send_message(
+                    channel_id,
+                    "Сейчас перерыв"
+                )
+                BREAK_MESSEAGE_ALREADY_SENTED = True
+            await asyncio.sleep(random.randint(0, RANDOM_SHIFT))
+            continue
+        screenshot_cat = get_claster_cat(screenshot)
+        caption = f"#{INCRIMINANT} | cat {screenshot_cat}"
         INCRIMINANT += 1
         await client.send_file(
             channel_id,
@@ -76,7 +159,8 @@ async def main():
 
         os.remove(screenshot)
 
-        delay = BASE_INTERVAL + random.randint(-RANDOM_SHIFT, RANDOM_SHIFT)
+        BREAK_MESSEAGE_ALREADY_SENTED = False
+        delay = BASE_INTERVAL +  RANDOM_SHIFT * (2*random.random()-1)**3
         print("delay =", delay)
         await asyncio.sleep(delay)
 
